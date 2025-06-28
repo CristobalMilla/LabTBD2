0f6dc82ad907c2d158efaa4e8e81639e95a81c2a
Cristobal Milla <cristobal.milla@usach.cl>
2025-06-27 22:08:05 -0400

    Arreglo query 3

--- Lab2BackEnd/src/main/java/Grupo4/Lab2/MongoDB/Services/LogsPedidosService.java
+++ Lab2BackEnd/src/main/java/Grupo4/Lab2/MongoDB/Services/LogsPedidosService.java
@@ -6,6 +6,7 @@ import Grupo4.Lab2.MongoDB.Repositories.LogsPedidosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -54,7 +55,30 @@ public class LogsPedidosService {
     public void deleteLog(long id){ repo.delete(id);}
 
     //Query 3:Contar cuántos pedidos tienen más de 3 cambios de estado en menos de 10 minutos.
-    public int countHistoriales10Minutos() {
-        return repo.countPedidosLog10Minutos();
+    public int countPedidosLog10Minutos() {
+        List<LogsPedidos> logsPedidos = repo.findAll();
+        int count = 0;
+        for (LogsPedidos log : logsPedidos) {
+            boolean pedidoTiene3Cambios = false;
+            List<EventoPedido> eventos = log.getEventos();
+            int totalEventos = eventos.size();
+            int menos10Minutos = 0;
+            for (int i=0; (i<totalEventos - 2); i++){
+                Instant instante1 = eventos.get(i).getTimestamp();
+                Instant instante2 = eventos.get(i+1).getTimestamp();
+                long minutesDiference = Duration.between(instante1, instante2).toMinutes();
+                if (minutesDiference<=10){
+                    menos10Minutos++;
+                }
+                if (menos10Minutos>=3){
+                    pedidoTiene3Cambios = true;
+                    break;
+                }
+            }
+            if(pedidoTiene3Cambios){
+                count++;
+            }
+        }
+        return count;
     }
 }
