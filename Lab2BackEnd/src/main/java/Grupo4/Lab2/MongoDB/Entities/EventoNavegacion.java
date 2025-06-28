716b182c026a76d046934241a02bf18c88760705
claubells <claudia.villa@usach.cl>
2025-06-27 22:27:16 -0400

    se crea contructores, getter y setter pa EventoNavegacion

--- Lab2BackEnd/src/main/java/Grupo4/Lab2/MongoDB/Entities/EventoNavegacion.java
+++ Lab2BackEnd/src/main/java/Grupo4/Lab2/MongoDB/Entities/EventoNavegacion.java
@@ -7,4 +7,37 @@ public class EventoNavegacion {
     private String tipo;       // ej: "busqueda", "click", "filtro"
     private String valor;      // nombre producto ej: "paracetamol"
     private Instant timestamp;
+
+    public EventoNavegacion() {
+    }
+
+    public EventoNavegacion(String tipo, String valor, Instant timestamp) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.timestamp = timestamp;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
 }
