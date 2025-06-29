04d4558d41da9c4424b546ad58f7ccb895a90bb8
Dante.stormblessed <alvaro.munoz.a@usach.cl>
2025-06-28 20:39:50 -0400

    Actualizar api de opiniones

--- Lab2Frontend/src/api/opiniones.js
+++ Lab2Frontend/src/api/opiniones.js
@@ -1,17 +1,22 @@
 import axiosInstance from "./axiosInstance";
 
 export const getOpinionById = async (id) => {
-  const response = await axiosInstance.get(`/opiniones/${id}`);
+  const response = await axiosInstance.get(`/opiniones/getById/${id}`);
   return response.data;
 };
 
 export const createOpinion = async (opinion) => {
-  const response = await axiosInstance.post("/opiniones", opinion);
+  const response = await axiosInstance.post("/opiniones/save", opinion);
   return response.data;
 };
 
 export const updateOpinion = async (id, opinion) => {
-  const response = await axiosInstance.put(`/opiniones/${id}`, opinion);
+  const response = await axiosInstance.put(`/opiniones/update/${id}`, opinion);
+  return response.data;
+};
+
+export const deleteOpinion = async (id) => {
+  const response = await axiosInstance.delete(`/opiniones/${id}`);
   return response.data;
 };
 
